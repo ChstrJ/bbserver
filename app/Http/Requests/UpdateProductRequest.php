48d0c94d64a6a6fab307b26aff8c55b74d164319
Chester <89991461+ChesterJaz@users.noreply.github.com>
2024-05-24 19:37:17 +0800

    push

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Http\Helpers\ProductCategories;
+use DB;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateProductRequest extends FormRequest
@@ -22,12 +23,24 @@ class UpdateProductRequest extends FormRequest
      */
     public function rules(): array
     {
-        $categories_length = \App\Http\Utils\ProductCategories::getCategories();
+        //$categories_length = \App\Http\Utils\ProductCategories::getCategories();
+       
         return [
             'category_id' => "sometimes|int|min:1|exists:categories,id",
             'name' => 'sometimes|string',
             'description' => 'sometimes|string',
-            'quantity' => 'sometimes|int|min:1|max:10000',
+            'quantity' => [
+                'sometimes',
+                'int',
+                'min:1',
+                'max:10000',
+                function ($attribute, $value, $fail) {
+                    $product = DB::table("products")->where("id", $this->id)->first();
+                    if ($product && $value <= $product->quantity) {
+                        $fail("The {$attribute} must be greater than the current quantity");
+                    }
+                 }
+            ],
             'srp' => 'sometimes|numeric|min:1|max:500000',
             'member_price' => 'sometimes|numeric|min:1|max:500000',
         ];
